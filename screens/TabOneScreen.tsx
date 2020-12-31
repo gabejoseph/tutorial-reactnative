388ce8c32ac77e97ca4e3ec8491327b6bf07c6d1
gabejoseph <gabriel.joseph4@gmail.com>
2020-12-31 13:54:59 -0500

    Hello World, application up and running

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Hello World!!!</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: 'bold', 
   },
   separator: {
     marginVertical: 30,
